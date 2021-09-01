0f324f0b9eca458d98d4fd3ba88856b03b0fd512
UnicornCodeQueen <75204601+UnicornCodeQueen@users.noreply.github.com>
2021-09-01 22:53:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("dc_fanchant_button").onclick = function () {
+    window.location.href = "dc_fanchants.html";
+}
+
+document.getElementById("dc_back_button").onclick = function () {
+    window.location.href = "index.html";
+}
